6c8df1a16c2eecadc79f33af3156eb83ddc5404e
marcelo <marcelo.melo@tce.pa.gov.br>
2024-06-19 10:01:22 -0300

    criado o upload de imagens

--- user_process.php
+++ user_process.php
@@ -26,10 +26,44 @@ if($type === "update"){
     $email = filter_input(INPUT_POST, "email");
     $bio = filter_input(INPUT_POST, "bio");
 
+    $user = new User();
+
     $userData->name = $name;
     $userData->lastname = $lastname;
     $userData->email = $email;
     $userData->bio = $bio;
+
+
+    //Upload da imagem
+    if(isset($_FILES["image"]) && !empty($_FILES["image"]["tmp_name"])){
+
+        $image = $_FILES["image"];
+
+        $imageTypes = ["image/png", "image/jpeg", "image/jpg"];
+        $jpgArray = ["image/jpeg", "image/jpg"];
+
+        //checagem de tipo de imagem
+        if(in_array($image["type"], $imageTypes)){
+            
+
+            //checar se é jpg
+            if(in_array($image, $jpgArray)){
+                $imageFile = imagecreatefromjpeg($image["tmp_name"]);
+
+            } else {
+                $imageFile = imagecreatefrompng($image["tmp_name"]);
+            }
+
+            $imageName = $user->imageGenerateName();
+
+            imagejpeg($imageFile, "./img/users/" . $imageName, 100);
+
+            $userData->image = $imageName;
+
+        } else {
+            $message->setMessage("Tipo de imagem inválido!", "error", "editprofile.php");
+        }
+    }
 
     $userDao->update($userData);
 
